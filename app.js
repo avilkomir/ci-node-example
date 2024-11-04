193101ea8c4f8f076914e2341498aef6dd8244b3
Kii4ka <evilkomir@gmail.com>
2024-11-03 21:16:48 -0500

    Add package-lock to gitignore

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, CI/CD World!');
+    res.send('Hello, CI World!');
 });
 
 module.exports = app;
